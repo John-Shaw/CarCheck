c65c169f0a1d623a60b6c017f790b0551fcfddca
John Shaw <bard2093@gmail.com>
2015-04-11 16:17:43 +0800

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    private int index;
+    private int endIndex;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        index = 0;
+        endIndex = 15;
+    }
+
+    private void fillText(int index){
+
+    }
+
+}
